f37c50191bf10f6b883cf00ff4e00ded9ad0e924
sachin shukla <sachinshukla232003@gmail.com>
2025-01-29 22:51:38 +0530

    Added Round Robin Algorithm using Linked list week 3 day 1

---
+++ Day 01 LinkedList/src/com/linkedlist/circularlinkedlist/roundrobinalgorithm/Main.java
@@ -0,0 +1,24 @@
+package com.linkedlist.circularlinkedlist.roundrobinalgorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        RoundRobinAlgorithm scheduler = new RoundRobinAlgorithm();
+
+        // Adding processes to the queue
+        scheduler.addProcess(1, 10, 1);
+        scheduler.addProcess(2, 5, 2);
+        scheduler.addProcess(3, 8, 1);
+
+        // Display processes
+        scheduler.displayProcesses();
+
+        // Simulate round-robin scheduling with time quantum of 4
+        System.out.println("\nSimulating Round Robin Scheduling:");
+        scheduler.simulateRoundRobin(4);
+
+        // Display remaining processes (if any)
+        System.out.println("\nRemaining Processes in the Queue:");
+        scheduler.displayProcesses();
+    }
+}
+
