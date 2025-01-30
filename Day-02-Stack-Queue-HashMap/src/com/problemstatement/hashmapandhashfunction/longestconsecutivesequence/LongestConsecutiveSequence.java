78f75f6f5374d245e25d7adbdda9bfe0dda7d582
sachin shukla <sachinshukla232003@gmail.com>
2025-01-30 16:09:36 +0530

    Added Longest Consecutive Sequence week 3 day 2

---
+++ Day-02-Stack-Queue-HashMap/src/com/problemstatement/hashmapandhashfunction/longestconsecutivesequence/LongestConsecutiveSequence.java
@@ -0,0 +1,14 @@
+package com.problemstatement.hashmapandhashfunction.longestconsecutivesequence;
+
+import java.util.HashMap;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutiveSequence(int arr[]){
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+    }
+    public static void main(String[] args) {
+        int arr[]={100,4,200,1,3,2};
+
+    }
+}
